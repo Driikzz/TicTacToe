220a2d8e1a1b2c563aa5b2905132b3aa71eee970
Rémi Salles <remisalles.12@gmail.com>
2022-11-16 17:01:46 +0100

    choixJoueur

--- jeux.py
+++ jeux.py
@@ -1,29 +1,72 @@
 
 tableau = []
+tableau.append(['-','-','-'])
+tableau.append(['-','-','-'])
+tableau.append(['-','-','-'])
 
-def choixJoueur():
-    choixJoueurs = input(("Tu souhaites écrire dans quelles case ?: "))
-    return choixJoueurs
+    
 
 def showBoard(): 
-    for i in range(3):
-        tableau.append([]) 
-        for j in range(3):
-            tableau[i].append(' ') 
-    for i in range(3): 
-        print(tableau[i])
-    return print(tableau[i])
+    for i in range(0,len(tableau)):
+        print(" | ".join(str(e) for e in tableau[i]))
+
+            
+    
+
+def choixJoueur():
+    caseDisponible = 9
+    test = False
+    while caseDisponible > 0: 
+        entreeJoueurs = input(("Tu souhaites écrire dans quelles case ?: "))
+        for x in range(0,len(tableau)):
+            for y in range(0,len(tableau[x])) : 
+                if tableau[x][y] == '-':
+                    print(caseDisponible)
+                    caseDisponible = caseDisponible- 1
+                    test = True
+                    break  
+                else:
+                    print("Erreur: case pleine essaie une autre case.")
+        
+        if test == True:
+            break
+    return entreeJoueurs
+
+    
 
 def game ():
-    showBoard()
-    choixJoueurs = choixJoueur()
     start = 1
-    
-    if choixJoueurs == "oui": 
-        tableau[0]= ('X')
+    while start == 1:
         showBoard()
-    else: 
-        print("NON")
+        choixJoueurs = choixJoueur()
+
+        for x in range(0,len(tableau)):
+            for y in range(0,len(tableau[x])): 
+
+                if choixJoueurs == "1": 
+                    tableau[0][0]= ('X')
+                if choixJoueurs == "2": 
+                    tableau[0][1]= ('X')
+                if choixJoueurs == "3": 
+                    tableau[0][2]= ('X')
+                if choixJoueurs == "4": 
+                    tableau[1][0]= ('X')   
+                if choixJoueurs == "5": 
+                    tableau[1][1]= ('X')           
+                if choixJoueurs == "6": 
+                    tableau[1][2]= ('X')
+                if choixJoueurs == "7": 
+                    tableau[2][0]= ('X')
+                if choixJoueurs == "8": 
+                    tableau[2][1]= ('X')  
+                if choixJoueurs == "9": 
+                    tableau[2][2]= ('X')
+
+        
+           
+       
+            
+            
 
 game()
 
