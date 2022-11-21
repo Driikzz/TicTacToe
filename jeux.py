5f754cff597fd7cb28634f431d11f254c5301420
Rémi Salles <remisalles.12@gmail.com>
2022-11-21 16:45:46 +0100

    test

--- jeux.py
+++ jeux.py
@@ -3,10 +3,13 @@ tableau = []
 tableau.append(['-','-','-'])
 tableau.append(['-','-','-'])
 tableau.append(['-','-','-'])
-winner = 0
-signeJoueur = ' '
-tourJoueurUn = False 
-tourJoueurDeux = False
+
+
+signeJoueur = 'X'
+signeJoueur = 'O'
+
+import os
+clear = lambda: os.system('cls')
 
 def showBoard(): 
     for i in range(0,len(tableau)):
@@ -32,13 +35,11 @@ def testing():
                     win = False
                     break
             if win:
-                print("gagné")
-                winner = 1
-                tourJoueurUn = False 
-                tourJoueurDeux = False
-                return tourJoueurUn and tourJoueurDeux
+                print("gagné ROW")
+                return win
 
 def checkColumns():
+        
         n = len(tableau)
         # checking columns
         for i in range(n):
@@ -48,11 +49,10 @@ def checkColumns():
                     win = False
                     break
             if win:
-                print("gagné")
-                winner = 1
-                tourJoueurUn = False 
-                tourJoueurDeux = False
-                return tourJoueurUn and tourJoueurDeux
+                start = 0
+                print("gagné Col ")
+                
+                return win
 
 def checkDiagonals():
         n = len(tableau)
@@ -63,18 +63,41 @@ def checkDiagonals():
                 win = False
                 break
         if win:
-            winner = 1
-            print("gagné")
-            return winner
+            print("gagné diag")
+            return win
+        
+        win = True
+        for i in range(n):
+            if tableau[i][n - 1 - i] != tableau:
+                win = False
+                break
+        if win:
+            return win
+        return False
+
+        for row in taleau:
+            for item in row:
+                if item == '-':
+                    return False
+        return True
+
+def checkEqual():
+        for row in tableau:
+            for item in row:
+                if item == '-':
+                    return False
+        return True
 
 
 def game ():
     start = 1
     choiceGame = input(("Tu souhaites jouer avec un ami, ou avec un bot ?: "))
-    signeJoueur = 'X' if randint(1,2) == 1 else 'O'
-    
+    signeJoueur = 'X'
+    restart = 0
     
     while start == 1:            
+        
+        
         if choiceGame == "ami":
             tourJoueurUn = True
             if tourJoueurUn == True:
@@ -82,45 +105,52 @@ def game ():
             else : 
                 print("Joueur 2 ") 
 
-
         while tourJoueurUn == True: 
             showBoard()
+            if signeJoueur == 'O':
+                signeJoueur = 'X'
+            else:
+                signeJoueur = 'O'
             print("C'est au tour de :"+ signeJoueur)
             choixJoueurs = (choixJoueur().split())
             for i in range(0,len(choixJoueurs)):
                 choixJoueurs[i] = int(choixJoueurs[i])
-            if(tableau[choixJoueurs[0]][choixJoueurs[1]]=="-"):
-                tableau[choixJoueurs[0]][choixJoueurs[1]] = signeJoueur
-                tourJoueurDeux = True
-                testing()
-                checkColumns()
-                checkDiagonals()
-                break   
-            else: 
-                print("Case occupé, essaie une autre !")
-            
+            while start == 1:
+                if(tableau[choixJoueurs[0]][choixJoueurs[1]]=="-"):
+                    tableau[choixJoueurs[0]][choixJoueurs[1]] = signeJoueur
+                    if testing():
+                        print("ROW")
+                        print("Le joueur " + signeJoueur +" a gagné ")
+                        start = 0
+                        tourJoueurUn == False
+                        restart = 1
+                    if checkColumns():
+                        print("COL")
+                        print("Le joueur " + signeJoueur +" a gagné ")
+                        start = 0
+                        tourJoueurUn == False
+                        restart = 1
+                    if checkDiagonals(): 
+                        print("Le joueur " + signeJoueur +" a gagné ")
+                        start = 0
+                        tourJoueurUn == False
+                        restart = 1
+                    if not checkEqual:
+                        print("Equal")
+                    if restart == 1 :
+                        choiceRestart = input("Vous voulez rejouer ?: ")
+                        if choiceRestart == "oui":
+                            clear()
+                            game()
+                            
 
-        while tourJoueurDeux == True:
-            if signeJoueur == 'X':
-                signeJoueur = 'O'
-            else:
-                signeJoueur = 'X'     
-            showBoard()
-            print("C'est au tour de :"+ signeJoueur) 
-            choixJoueursDeux = (choixJoueurDeux().split())
-            for i in range(0,len(choixJoueurs)):
-                choixJoueursDeux[i] = int(choixJoueursDeux[i])
-            if(tableau[choixJoueursDeux[0]][choixJoueursDeux[1]]=="-"):
-                tableau[choixJoueursDeux[0]][choixJoueursDeux[1]] = signeJoueur
-                tourJoueurUn = True
-                testing()
-                checkColumns()
-                checkDiagonals()
-                if winner == 1 :
-                    print(signeJoueur + "Bien joué tu as gagné !")
-                break   
-            else: 
-                print("Case occupé, essaie une autre !")
+                        else:
+                            break
+                    break   
+                else: 
+                    print("Case occupé, essaie une autre !")
+                    break
+            
 
 game()
 
